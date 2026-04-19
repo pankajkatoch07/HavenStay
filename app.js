35caab38c19b927c852b85e46fd57cccb0a43fef
pankajkatoch07 <pankajkatochofficial@gmail.com>
2026-04-19 20:28:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/HavenStay');
+}
+
+main().then(()=>{console.log("Connected to Database")}).catch(err => console.log(err));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true})); //parses req.params
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+//INDEX ROUTE
+app.get("/listings",async (req,res) => {
+    const alllistings = await Listing.find();
+    res.render("listings/index.ejs",{alllistings}) 
+});
+
+//New Route
+app.get("/listings/new" , (req,res) => {
+    res.render("listings/new.ejs");
+});
+
+//SHOW ROUTE
+app.get("/listings/:id" ,async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+//Create Route
+app.post("/listings",async (req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit",async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs" ,{listing});
+});
+
+//Update Route
+app.put("/listings/:id",async (req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete Route
+app.delete("/listings/:id" , async(req,res) => {
+    let {id} = req.params;
+    let deletedLisitng = await Listing.findByIdAndDelete(id);
+    console.log(deletedLisitng);
+    res.redirect("/listings");
+});
+
+app.get("/",(req,res) => {
+    res.send("Welcome to the home page");
+});
+
+// app.get("/testlisting",async (req,res)=>{
+//     let sampleListing = new Listing ({
+//         title : "My new villa",
+//         description: "by the beach",
+//         price: 12000,
+//         location: "California",
+//         country: "Usa"
+//     });
+//     await sampleListing.save().catch(err => console.log(err));
+//     console.log("sample was saved");
+//     res.send("Successful Testing");
+// });
+
+app.listen(8080,() => {
+    console.log("Listening to Port 8080");
+});
+
---
+++ init/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/HavenStay');
+}
+
+main().then(()=>{console.log("Connected to Database")}).catch(err => console.log(err));
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
+
+initDB();
